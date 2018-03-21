11a14e23cd1488dd4e250a8b105bf0f398900041
julilp95 <julylp95@gmail.com>
2018-03-21 14:42:23 +0100

    He realizado la clase cliente

--- Biblioteca/src/Cliente.java
+++ Biblioteca/src/Cliente.java
@@ -1,4 +1,30 @@
 
 public class Cliente {
-
+	
+	private String nombre;
+	private String dni;
+	
+	Cliente (String nombre, String dni){
+		this.nombre = nombre;
+		this.dni = dni;
+	}
+	
+	//NOMBRE//	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	//DNI//
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
 }
