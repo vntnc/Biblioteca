457be19b756e2cba5440cd5dbdbdb8d48cc043db
vntnc <valentinwassaa@gmail.com>
2018-03-21 14:31:55 +0100

    libro en proceso, clase cliente creada

---
+++ Biblioteca/src/Libro.java
@@ -0,0 +1,19 @@
+
+public class Libro {
+
+	private final String titulo;
+	private final String autor;
+	private final Integer fechaPublicacion;
+	private final Double precio;
+	
+	Libro(String titulo, String autor, Integer fechaPublicacion, Double precio){
+		this.titulo = titulo;
+		this.autor = autor;
+		this.fechaPublicacion = fechaPublicacion;
+		this.precio = precio;
+	}
+	
+	
+	
+	
+}
